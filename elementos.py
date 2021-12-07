d98995b8d222cdcdd94b9e7f6f3ad8f686d65c46
Odilon Cavalcante Junior <odiloncavalcantejunior@gmail.com>
2021-12-06 21:22:05 -0300

    Testando a branch

--- elementos.py
+++ elementos.py
@@ -1,3 +1,6 @@
 # Lista de palavras
 with open('palavras.txt', 'r', encoding='utf-8') as file:
     lista_palavras = file.read().replace(' ', '').replace('\n', '').split(',')
+
+
+# testando as branchss
