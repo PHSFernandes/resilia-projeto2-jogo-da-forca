71ab215ff5f23d6ae7843b812c6daccc70a8e31d
Paulo <fernandes.phs@gmail.com>
2021-12-11 20:40:41 +0000

    adicionado desenhos da forca

--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,5 @@
 from os import system, name
-
+from time import sleep
 
 def screen_clear():
     """Esta função limpa o terminal"""
@@ -8,3 +8,65 @@ def screen_clear():
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+
+
+
+def desenha_forca(erros):
+    print("  ___     ")
+    sleep(0.1)
+    print(" |/      |    ")
+    sleep(0.1)
+
+    if(jogadores[vez]['erros'] == 1):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |            ")
+        sleep(0.1)
+        print(" |            ")
+        sleep(0.1)
+        print(" |            ")
+
+    if(jogadores[vez]['erros'] == 2):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |      \|    ")
+        sleep(0.1)
+        print(" |            ")
+        sleep(0.1)
+        print(" |            ")
+
+    if(jogadores[vez]['erros'] == 3):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |      \|/   ")
+        sleep(0.1)
+        print(" |            ")
+        sleep(0.1)
+        print(" |            ")
+
+    if(jogadores[vez]['erros'] == 4):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |      \|/   ")
+        sleep(0.1)
+        print(" |       |    ")
+        sleep(0.1)
+        print(" |            ")
+
+    if(jogadores[vez]['erros'] == 5):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |      \|/   ")
+        sleep(0.1)
+        print(" |       |    ")
+        sleep(0.1)
+        print(" |      /     ")
+
+    if (jogadores[vez]['erros'] == 6):
+        print(" |      (_)   ")
+        sleep(0.1)
+        print(" |      \|/   ")
+        sleep(0.1)
+        print(" |       |    ")
+        sleep(0.1)
+        print(" |      / \   ")
