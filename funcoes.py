2ea5f2191ed20406ecea97c98b9afaf5dcac4c73
Gabriel Cincinato <gabrielcicinato47@gmail.com>
2021-12-14 12:23:43 -0300

    Adicionada a opção de jogar novamente

--- funcoes.py
+++ funcoes.py
@@ -20,6 +20,7 @@ def print_slow(str):
         sys.stdout.flush()
         time.sleep(0.1)
 
+
 def sorteio_palavra():
     sorteio = random.choice(elementos.lista_palavras).lower()
     return sorteio
@@ -33,3 +34,51 @@ def seleciona_quantidades_de_jogadores():
         quantidade_jogadores = input("Selecione um valor válido (2 a 4): ")
 
     return int(quantidade_jogadores)
+
+
+def preencher_lista_jogadores(quantidade):
+    lista_jogadores = []
+    for n in range(quantidade):
+        nome = input(f"Escolha o nome do jogador {n + 1}: ").title()
+        while nome == "" or nome.isspace():
+            nome = input(f"Preencha o nome do jogador {n + 1}: ").title()
+        else:
+            lista_jogadores.append(Jogador(nome))
+
+    return lista_jogadores
+
+
+def jogador_acertou(jogador_atual):
+    jogador_atual.substituir_adivinhacao_por_letra()
+    jogador_atual.acertos = len(jogador_atual.palavra) - jogador_atual.adivinhacao.count("_")
+
+
+def jogador_venceu(jogador_atual):
+    screen_clear()
+    jogador_atual.template()
+    print(f"{jogador_atual.nome} venceu o jogo, parabéns!")
+    time.sleep(1)
+
+
+def jogador_errou(jogador_atual):
+    jogador_atual.vidas -= 1
+    screen_clear()
+    jogador_atual.template()
+    print("Você errou")
+    time.sleep(1)
+
+
+def verificar_chute_valido(jogador_atual):
+    if len(jogador_atual.chute) > 1 or jogador_atual.chute.isalpha() is False:
+        print("Escolha APENAS uma letra!")
+
+    elif jogador_atual.chute.upper() in jogador_atual.letras_erradas or jogador_atual.chute in jogador_atual.adivinhacao:
+        print("Você já escolheu esta letra. Tente outra.")
+
+    time.sleep(1)
+
+
+def jogador_sem_vida(jogador_atual, perdedores):
+    print(f"{jogador_atual.nome} está fora do jogo. A palavra era {jogador_atual.palavra.upper()}")
+    perdedores.append(jogador_atual)
+    time.sleep(2)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 from jogadores import Jogador
-from funcoes import print_slow, screen_clear, seleciona_quantidades_de_jogadores
+from funcoes import *
 from elementos import logo
+
 
 def preencher_lista_jogadores(quantidade):
     lista_jogadores = []
@@ -14,15 +15,18 @@ def preencher_lista_jogadores(quantidade):
 
     return lista_jogadores
 
+
 def jogador_acertou(jogador_atual):
     jogador_atual.substituir_adivinhacao_por_letra()
     jogador_atual.acertos = len(jogador_atual.palavra) - jogador_atual.adivinhacao.count("_")
+
 
 def jogador_venceu(jogador_atual):
     screen_clear()
     jogador_atual.template()
     print(f"{jogador_atual.nome} venceu o jogo, parabéns!")
     time.sleep(1)
+
 
 def jogador_errou(jogador_atual):
     jogador_atual.vidas -= 1
@@ -31,22 +35,24 @@ def jogador_errou(jogador_atual):
     print("Você errou")
     time.sleep(1)
 
+
 def verificar_chute_valido(jogador_atual):
     if len(jogador_atual.chute) > 1 or jogador_atual.chute.isalpha() is False:
         print("Escolha APENAS uma letra!")
-        
+
     elif jogador_atual.chute.upper() in jogador_atual.letras_erradas or jogador_atual.chute in jogador_atual.adivinhacao:
         print("Você já escolheu esta letra. Tente outra.")
 
     time.sleep(1)
+
 
 def jogador_sem_vida(jogador_atual, perdedores):
     print(f"{jogador_atual.nome} está fora do jogo. A palavra era {jogador_atual.palavra.upper()}")
     perdedores.append(jogador_atual)
     time.sleep(2)
 
+
 def jogar():
-    print_slow(logo)
     quantidade_jogadores = seleciona_quantidades_de_jogadores()
     lista_jogadores = preencher_lista_jogadores(quantidade_jogadores)
     print(lista_jogadores)
@@ -85,4 +91,9 @@ def jogar():
                         time.sleep(1)
                     break
 
-jogar()
+
+print_slow(logo)
+jogar_novamente = "s"
+while jogar_novamente == "s":
+    jogar()
+    jogar_novamente = input("Deseja jogar novamente? S/N ").lower()
